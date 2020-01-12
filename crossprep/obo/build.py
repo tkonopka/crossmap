5d990a1fa4caa00e48599c0d9db727c02c9e393c
tkonopka <tokonopka@gmail.com>
2020-01-12 11:05:47 +0000

    obo build - include grandparents for nodes without a description

--- crossprep/obo/build.py
+++ crossprep/obo/build.py
@@ -70,7 +70,15 @@ class OboBuilder:
         """transfer data from obo into a dictionary"""
 
         result = dict()
+
+        # shortcuts
         obo, hits = self.obo, self.hits
+        content = self.content
+        aux_parents, aux_ancestors = self.aux_parents, self.aux_ancestors
+        aux_synonyms, aux_children = self.aux_synonyms, self.aux_children
+        aux_siblings = self.aux_siblings
+        aux_top, aux_comments = self.aux_top, self.aux_comments
+
         for id in obo.ids():
             if id not in hits:
                 continue
@@ -79,28 +87,34 @@ class OboBuilder:
             data_pos = []
             data_neg = []
             metadata = dict()
-            if self.aux_comments:
+            if aux_comments:
                 data_pos.append(comment(term))
-            if self.aux_synonyms:
+            if aux_synonyms:
                 data_pos.append(synonyms(term))
-            if self.aux_top:
+            if aux_top:
                 for ancestor in obo.ancestors(id):
                     if ancestor in self.top:
                         data_pos.append(obo.terms[ancestor].name)
-            if self.aux_ancestors:
+            if aux_ancestors:
                 metadata["ancestors"] = []
                 for ancestor in obo.ancestors(id):
                     metadata["ancestors"].append(ancestor)
-                    data_pos.append(self.content(obo.terms[ancestor]))
-            elif self.aux_parents:
+                    data_pos.append(content(obo.terms[ancestor]))
+            elif aux_parents:
                 metadata["parents"] = []
                 for parent in obo.parents(id):
                     metadata["parents"].append(parent)
-                    data_pos.append(self.content(obo.terms[parent]))
-            if self.aux_siblings:
+                    data_pos.append(content(obo.terms[parent]))
+            if aux_parents and (term.data is None or "def" not in term.data):
+                grandparents = set()
+                for parent in obo.parents(id):
+                    grandparents.update(obo.parents(parent))
+                for grandparent in grandparents:
+                    data_pos.append(content(obo.terms[grandparent]))
+            if aux_siblings:
                 for sibling in obo.siblings(id):
                     data_neg.append(obo.terms[sibling].name)
-            if self.aux_children:
+            if aux_children:
                 for child in obo.children(id):
                     data_neg.append(obo.terms[child].name)
 
